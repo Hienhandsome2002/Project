a6c92ffb3123765dcb8c6e37bedcb26d201ff8e7
Hienhandsome2002 <2051063835@e.tlu.edu.vn>
2022-05-17 23:21:45 +0700

    add icons social

--- src/app/Profile/Home/Home.js
+++ src/app/Profile/Home/Home.js
@@ -1,5 +1,11 @@
 import React, { memo, useState, useEffect } from "react";
 import './Home.css';
+import { FaFacebookSquare,
+    FaTwitter,
+    FaGooglePlusG,
+    FaGithub,
+    FaInstagram,
+    FaSkype, } from "react-icons/fa";
 function Home() {
     return (
         <div className="home">
@@ -13,6 +19,27 @@ function Home() {
                     I'm a Manila based <span>Webdesigner</span> creating awesome and effective visual identities for companies of all sizes around the globe
                     .Let's <span> start scrolling</span> and  learn more <span>about me</span>
                 </h3>
+                </div>
+                <hr></hr>
+                <div className="socialIcons">
+                    <div className="ContainerIcons">
+                    <FaFacebookSquare className="Icons"></FaFacebookSquare>
+                    </div>
+                    <div className="ContainerIcons">
+                    <FaTwitter className="Icons"></FaTwitter>
+                    </div>
+                    <div className="ContainerIcons">
+                    <FaGooglePlusG className="Icons"></FaGooglePlusG>
+                    </div>
+                    <div className="ContainerIcons">
+                    <FaGithub className="Icons"></FaGithub>
+                    </div>
+                    <div className="ContainerIcons">
+                    <FaInstagram className="Icons"></FaInstagram>
+                    </div>
+                    <div className="ContainerIcons">
+                    <FaSkype className="Icons"></FaSkype>
+                    </div>
                 </div>
 
             </div>
