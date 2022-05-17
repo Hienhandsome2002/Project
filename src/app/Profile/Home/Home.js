e7eef19d61eeac0687fb0821251edda43e19b815
Hienhandsome2002 <2051063835@e.tlu.edu.vn>
2022-05-17 17:22:44 +0700

    supplement for Home

--- src/app/Profile/Home/Home.js
+++ src/app/Profile/Home/Home.js
@@ -3,11 +3,18 @@ import './Home.css';
 function Home() {
     return (
         <div className="home">
-           
+
             <div className="banner-text">
                 <h1 className="headText">
                     I'm Tuan Hien
                 </h1>
+                <div className="container-subText">
+                <h3 className="subText">
+                    I'm a Manila based <span>Webdesigner</span> creating awesome and effective visual identities for companies of all sizes around the globe
+                    .Let's <span> start scrolling</span> and  learn more <span>about me</span>
+                </h3>
+                </div>
+
             </div>
         </div>
     )
