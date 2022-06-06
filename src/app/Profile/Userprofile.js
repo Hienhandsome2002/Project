3081d638b84e266a8ccf316277d93f5921845ad2
Hienhandsome2002 <99744161+Hienhandsome2002@users.noreply.github.com>
2022-06-06 22:58:46 +0700

    inital About

--- src/app/Profile/Userprofile.js
+++ src/app/Profile/Userprofile.js
@@ -1,10 +1,12 @@
 import Header from "./Header/Header";
 import Home from "./Home/Home";
+import Aboutme from "./Aboutme/Aboutme";
 function Userprofile() {
     return(
         <div className="container">
             <Header />
             <Home />
+            <Aboutme />
         </div>
     )
 
